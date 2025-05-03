739892b677ae14fa75e95be4ff3abee9bf6c77f1
東門　博飛 <s1422162@mail.meio-u.ac.jp>
2025-05-04 04:42:19 +0900

    favicon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -9,6 +9,10 @@ const inter = Inter({ subsets: ['latin'] });
 export const metadata: Metadata = {
   title: '名桜大学祭 2025 | 公式サイト',
   description: '11月開催の名桜大学祭2025の公式ウェブサイトです。イベント情報、出店情報、協賛のお知らせなどを掲載しています。',
+  icons: {
+    icon: '/icon.png', 
+    apple: '/apple-icon.png',
+  },
 };
 
 export default function RootLayout({
